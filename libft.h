2eca79e701513126acb298a63f763591f44b351e
Amine <ael-mank@student.42.fr>
2023-11-07 22:53:43 +0100

    aa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 08:50:38 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/07 14:41:38 by ael-mank         ###   ########.fr       */
+/*   Updated: 2023/11/07 22:53:29 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ int		ft_tolower(int c);
 void	*ft_memset(void *ptr, int value, size_t size);
 void	fr_bzero(void *ptr, size_t n);
 char	*strchr(const char *str, int c);
+char	*strrchr(const char *str, int c);
+int	ft_strncmp( const char *first, const char *second, size_t length );
 
 #endif
