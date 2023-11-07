ca11b36506c7a38fcef1eb7f1f04d74bdeed4492
ael-mank <waz@LAPTOP-706UKDDA>
2023-11-07 22:53:43 +0100

    aa

---
+++ ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 14:51:46 by ael-mank          #+#    #+#             */
+/*   Updated: 2023/11/07 16:23:08 by ael-mank         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+
+int	ft_strncmp( const char *first, const char *second, size_t length )
+{
+	size_t	i;
+
+	i = 0;
+	while (i < length)
+	{
+		if (first[i] != second[i] || first[i] == '\0' || second[i] == '\0')
+			return (first[i] - second[i]);
+		i++;
+	}
+	return (0);
+}
