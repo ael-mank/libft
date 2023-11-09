65b07d58eb1676c44503253971838f203d2af1d9
ael-mank <waz@LAPTOP-706UKDDA>
2023-11-09 19:31:53 +0100

    ok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:14:51 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/08 15:43:11 by ael-mank         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:16:11 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t count)
 {
 	size_t	i;
 
+	if ((dest == NULL) || (src == NULL))
+		return (NULL);
 	i = 0;
 	while (i < count)
 	{
