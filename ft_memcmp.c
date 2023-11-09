089417ee4f4335b82377cbe1e29bc5578fde2c13
Amine El Mankouri <ael-mank@paul-f3Ar2s1.clusters.42paris.fr>
2023-11-09 15:46:52 +0100

    jssko

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:19:32 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/09 14:32:45 by ael-mank         ###   ########.fr       */
+/*   Updated: 2023/11/09 15:31:58 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	ft_memcmp(const void *ptr1, const void *ptr2, size_t size)
 	if (p1 == NULL || p2 == NULL)
 		return (0);
 	i = 0;
-	
 	while (i < size)
 	{
 		if (p1[i] != p2[i])
