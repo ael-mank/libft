b109ee8aa81e5f3af43bf78a875b321039a9e250
Amine <ael-mank@student.42.fr>
2023-11-09 13:56:16 +0100

    rhhrfh

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 08:50:38 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/09 12:33:56 by ael-mank         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:56:07 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,14 @@ int		ft_strlen(char *str);
 int		ft_isalnum(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_strncmp(const char *first, const char *second, size_t length);
 void	*ft_memset(void *ptr, int value, size_t size);
 void	ft_bzero(void *ptr, size_t n);
-char	*ft_strchr(const char *str, int c);
-char	*ft_strrchr(const char *str, int c);
-int		ft_strncmp(const char *first, const char *second, size_t length);
 void	*ft_memcpy(void *dest, const void *src, size_t count);
 void	*ft_memmove(void *dest, const void *src, size_t size);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 size_t	ft_strlcpy(char	*dest, char	*src, size_t destsize);
+size_t	ft_strlcat(char *dest, char *src, size_t destsize);
 
 #endif
