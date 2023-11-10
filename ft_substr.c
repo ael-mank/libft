e7d258928fdcdc8a0cdad051fbacc26f5051d7a1
Amine <ael-mank@student.42.fr>
2023-11-10 22:25:17 +0100

    sayer

---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 19:03:14 by ael-mank          #+#    #+#             */
+/*   Updated: 2023/11/10 22:10:35 by ael-mank         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	j;
+
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = start;
+	j = 0;
+	while (i < (long unsigned int)ft_strlen((char *)s) && j < len)
+		sub[j++] = s[i++];
+	sub[j] = '\0';
+	return (sub);
+}
