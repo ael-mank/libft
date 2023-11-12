f9c2052147f9f048dc0eb6bc3fda212404b664b6
Amine El Mankouri <ael-mank@paul-f5Br3s2.clusters.42paris.fr>
2023-11-12 15:03:32 +0100

    putnbr

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 09:51:38 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/12 14:14:00 by ael-mank         ###   ########.fr       */
+/*   Created: 2023/11/12 13:58:26 by ael-mank          #+#    #+#             */
+/*   Updated: 2023/11/12 14:09:10 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t size)
+void	ft_putchar_fd(char c, int fd)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
-
-	i = 0;
-	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	if (d == NULL || s == NULL)
-		return (0);
-	if (d > s)
-		while (size-- > 0)
-			d[size] = s[size];
-	else
-	{
-		while (i < size)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	}
-	return (dest);
+	write(fd, &c, 1);
 }
