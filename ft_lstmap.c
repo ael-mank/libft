aef25ec977f1ad7a21c41710b2753beb86ed2cde
Amine El Mankouri <ael-mank@paul-f3Ar3s9.clusters.42paris.fr>
2023-11-15 15:55:19 +0100

    ouai

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:23:53 by ael-mank          #+#    #+#             */
-/*   Updated: 2023/11/15 13:29:36 by ael-mank         ###   ########.fr       */
+/*   Updated: 2023/11/15 15:53:39 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*newobj;
 
-	while(lst)
+	if (!lst || !f || !del)
+		return (NULL);
+	while (lst)
 	{
 		newobj = ft_lstnew(f(lst->content));
 		if (!newobj)
